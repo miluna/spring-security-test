9c52840749fb4af9ed83acba335d907bce6e3080
Miguel Angel Luna <miluna1994@gmail.com>
2018-09-13 19:46:38 +0200

    initializr

---
+++ src/main/java/com/miluna/security/securitytestmiluna/SecurityTestMilunaApplication.java
@@ -0,0 +1,12 @@
+package com.miluna.security.securitytestmiluna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTestMilunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTestMilunaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/miluna/security/securitytestmiluna/SecurityTestMilunaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.miluna.security.securitytestmiluna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityTestMilunaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
